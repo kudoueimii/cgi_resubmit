35b158431efd186c131190faf5deaf6142ef09cc
bargholz eimii <818_77_@ezweb.ne.jp>
2022-09-16 17:12:51 +0900

    commited for CGI test

---
+++ test.rb
@@ -0,0 +1,19 @@
+require 'webrick'
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+
+server.mount('/test1', WEBrick::HTTPServlet::ERBHandler, 'test1.html.erb')
+
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+server.start
+
